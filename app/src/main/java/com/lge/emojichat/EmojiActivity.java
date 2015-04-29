c177eb6aef619ff3ef50693512523ad6bb4697a4
hyunsoo.yoon <hyunsoo.yoon@lge.com>
2015-04-29 15:18:09 +0900

    test for git push

--- app/src/main/java/com/lge/emojichat/EmojiActivity.java
+++ app/src/main/java/com/lge/emojichat/EmojiActivity.java
@@ -8,7 +8,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.FrameLayout;
-
 import com.lge.emojichat.constants.Constants;
 import com.lge.emojichat.util.EmojiUtils;
 
